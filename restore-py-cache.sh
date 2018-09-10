e0cb15a322727ea8850f0436aca98c0e84ee5759
Goran Kurel <38796068+rtgkurel@users.noreply.github.com>
2018-09-10 15:55:25 +0200

    Create restore-py-cache.sh

---
+++ restore-py-cache.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+py_ver=${1:-'3.7.0'}
+py_cache_archive="$SEMAPHORE_CACHE_DIR/py$py_ver-cache.tar.gz"
+
+# Install pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+echo 'export PATH="/home/runner/.pyenv/bin:$PATH"' | tee -a ~/.bash_profile
+echo 'eval "$(pyenv init -)"' | tee -a ~/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' | tee -a ~/.bash_profile
+source ~/.bash_profile
+
+# Restore the Python version from cache, if exists
+if [ -e $py_cache_archive ]; then
+  echo "Restoring Python $py_ver installation cache..."
+  tar -xf $py_cache_archive -C $SEMAPHORE_CACHE_DIR
+  rm -rf ~/.pyenv/versions/$py_ver
+  cp -r $SEMAPHORE_CACHE_DIR/py$py_ver ~/.pyenv/versions
+  echo "Python $py_ver installation cache restored."
+else
+  echo "Cacheed Python $py_ver installation not found, downloading..."
+  # install the prerequisites
+  sudo apt-get update
+  sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdm-dev libc6-dev libbz2-dev zlib1g-dev openssl libffi-dev python3-dev python3-setuptools wget
+
+  pyenv install $py_ver
+fi
+
+pyenv global $py_ver
